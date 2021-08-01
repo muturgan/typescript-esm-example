6909c8e3e2baba7d104132b3ef95dd65f2d450cf
Andrey Sakharov <sakharov.a@n-t.io>
2021-08-01 11:23:58 +0300

    just add some typings

--- src/utils/say_hallo.ts
+++ src/utils/say_hallo.ts
@@ -1,3 +1,3 @@
 import { HELLO, WORLD } from '../constants';
 
-export const sayHello = () => console.log(`${HELLO} ${WORLD}!`);
+export const sayHello = (): void => console.log(`${HELLO} ${WORLD}!`);
